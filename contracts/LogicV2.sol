0f97c30703aa44443edd5fd46d0c58f7b6ba79b8
Hans <hansvekoni@gmail.com>
2024-12-27 11:51:22 -0300

    Create: contract LogicV2

---
+++ contracts/LogicV2.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+// contracts/LogicV2.sol
+pragma solidity ^0.8.0;
+
+contract LogicV2 {
+    uint public number;
+
+    function setNumber(uint _number) public {
+        number = _number * 2; // Cambiamos la lógica
+    }
+
+    function getDouble() public view returns (uint) {
+        return number * 2;
+    }
+}
